e1b18e5c233cc0ccffcc30abc4e1c08f0e3f2969
Akhand <akhand11092005@gmail.com>
2025-08-24 07:44:21 +0530

    Create reset_machine_id.sh

---
+++ reset_machine_id.sh
@@ -0,0 +1,19 @@
+#ita a shebang
+#!/bin/bash
+
+# it'll display the current machine-id
+echo -n "Current machine-ID: "
+cat /etc/machine-id
+
+# it'll backup current machine-id to /etc/machine-id.bak file..
+sudo cp /etc/machine-id /etc/machine-id.bak
+
+# it'' erase the current machine-id file (this is a destructive step)
+sudo truncate -s 0 /etc/machine-id
+
+# generate a new machine-id
+sudo systemd-machine-id-setup
+
+# display the new machine-id 
+echo -n "New machine-ID: "
+cat /etc/machine-id
